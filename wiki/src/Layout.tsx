c9e9116b95a41c3410f703f2802597cfad569841
oleg3790 <oleg.krysko@outlook.com>
2019-06-07 13:59:57 -0500

    Banner work

--- wiki/src/Layout.tsx
+++ wiki/src/Layout.tsx
@@ -4,8 +4,10 @@ import { Container, Row, Col } from 'react-bootstrap';
 export default function Layout(props: any) {
     return (
         <Container fluid={true}>
-            <Row>
-                <Col id="main-banner"></Col>
+            <Row id="main-banner">
+                <Col xl={{ span: 3, offset: 9 }} className="p-2">
+                    <h1 id="main-title">Oleg Krysko's Tech Wiki</h1>
+                </Col>
             </Row>
             {props.children}
         </Container>
