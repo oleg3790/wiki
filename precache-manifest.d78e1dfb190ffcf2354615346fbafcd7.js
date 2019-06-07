c4c6df7baf2a064a43df7cac9aa3892cc624872c
oleg3790 <oleg.krysko@outlook.com>
2019-06-07 11:37:39 -0500

    Updates

---
+++ precache-manifest.d78e1dfb190ffcf2354615346fbafcd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca519f9e5f5f38f66dd76463b32ee3bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb8f9bf1c9ba8804c3b2",
+    "url": "/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "5880496430f1e1b7c4ca",
+    "url": "/static/js/2.812eb2e6.chunk.js"
+  },
+  {
+    "revision": "fb8f9bf1c9ba8804c3b2",
+    "url": "/static/js/main.f36d74a0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
