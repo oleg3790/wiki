a6c220dab55602df944881956a0d1b997e2dd211
oleg3790 <oleg.krysko@outlook.com>
2019-06-14 16:23:55 -0500

    Add test service and implementation to pull from GH API

--- wiki/src/pages/dot_net_tech/DotNetTechnologies.tsx
+++ wiki/src/pages/dot_net_tech/DotNetTechnologies.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import GitHubContentService from '../../commons/GitHubContentService';
 
-const DotNetTechnologies: React.FC = () => {
-    return (
-        <div></div>
-    );
+class DotNetTechnologies extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    async componentDidMount() {
+        const service = new GitHubContentService()
+        const r = await service.getSiteContents();
+    }
+
+    render() {
+        return <div></div>;
+    }
 }
 
 export default DotNetTechnologies;
