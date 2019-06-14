1381f274b8589de7c7ab5996334a75f305d1b1c5
oleg3790 <oleg.krysko@outlook.com>
2019-06-14 12:26:14 -0500

    Restyle site banner

--- wiki/src/Layout.tsx
+++ wiki/src/Layout.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import NavigationCard from './NavigationCard';
+import title from './images/title.png';
 import logo from './images/logo.png';
  
 export default function Layout(props: any) {
     return (
         <Container fluid={true}>
             <Row id="main-banner">
-                <Col xl={{ span: 3 }} className="p-1 pl-5">
-                    <img src={logo} width="240px" height="55px"/>
+                <Col xl={{ span: 6 }} className="p-1 pl-5">
+                    <img id="main-logo" src={logo} width="50px" height="50px" alt="logo"/>
+                    <h3 id="main-title" className="d-inline-block">Technological Wiki</h3>
                 </Col>
             </Row>
             <Row>
