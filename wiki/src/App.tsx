0923c2eadd327de09cceaf942196d40a243a70d3
oleg3790 <oleg.krysko@outlook.com>
2019-06-20 12:29:10 -0500

    Move mappers to seperate file & cleanup :recycle:

--- wiki/src/App.tsx
+++ wiki/src/App.tsx
@@ -1,51 +1,38 @@
 import React from 'react';
 import './styles/App.scss';
-import { Route, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Layout from './Layout';
 import GitHubContentService from './commons/GitHubContentService';
-import NodeDetails from './commons/NodeDetails';
+import { mapRoutes } from './commons/ContentTreeMapper';
 
 interface IAppState {
-    content: object|null;
+    contentTree: object|null;
 }
 
 export default class App extends React.Component<any, IAppState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            content: null
+            contentTree: null
         }
     }
 
     async componentDidMount() {
         const ghContentService = new GitHubContentService();
         const tree = await ghContentService.getSiteContentTree();
-        this.setState({ content: tree });
+        this.setState({ contentTree: tree });
     }
 
     render() {
-        const { content } = this.state;
+        const { contentTree } = this.state;
         return (
             <HashRouter basename="/">
-                <Layout navTree={content}>
+                <Layout contentTree={contentTree}>
                     <div>
-                        {content && this._getRoutes(content, [])}
+                        {contentTree && mapRoutes(contentTree)}
                     </div>
                 </Layout>
             </HashRouter>
         );
-    }
-
-    private _getRoutes = (content: object, initialRoutes: JSX.Element[]): JSX.Element[] => {
-        Object.entries(content).forEach(([key, value]) => {
-            if (typeof value === 'string') {
-                return initialRoutes;
-            }
-            initialRoutes.push(
-                <Route key={key} path={`/${value.UrlPath ? value.UrlPath : "error"}`} 
-                       render={(routeProps) => <NodeDetails {...routeProps} contentUrl={value.DownloadUrl ? value.DownloadUrl : null}/>}/>);
-            return this._getRoutes(value, initialRoutes);
-        });
-        return initialRoutes;
     }
 }
