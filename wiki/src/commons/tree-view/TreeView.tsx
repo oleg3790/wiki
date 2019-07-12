4192f76d9778ca51a73c583b16d73436cdcdc2dd
Oleg Krysko <oleg.krysko@outlook.com>
2019-07-11 19:43:42 -0500

    Add ContentTree object and interface and implement

---
+++ wiki/src/commons/tree-view/TreeView.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import ContentTree from '../ContentTree';
+
+
+interface ITreeViewProps {
+    contentTree: ContentTree
+}
+
+export default class TreeView extends React.Component<ITreeViewProps, {}> {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
