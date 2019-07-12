f9d7ae52b1271c981b7b985c77313fc46f0ed559
Oleg Krysko <oleg.krysko@outlook.com>
2019-07-11 22:34:15 -0500

    Add collapse icons to tree

--- wiki/src/commons/tree-view/TreeNode.tsx
+++ wiki/src/commons/tree-view/TreeNode.tsx
@@ -1,28 +1,47 @@
 import * as React from 'react';
+import '../../styles/TreeView.scss';
 import { Link } from 'react-router-dom';
 import ContentTree from '../ContentTree';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 interface ITreeNodeProps {
     childNodes: ContentTree[];
     className?: string;
 }
 
-export default class TreeNode extends React.Component<ITreeNodeProps, {}> {  
+interface ITreeNodeState {
+    collapsed: boolean
+}
+
+export default class TreeNode extends React.Component<ITreeNodeProps, ITreeNodeState> {  
     constructor(props: ITreeNodeProps) {
         super(props);
+
+        this.state = {
+            collapsed: false
+        }
     }
     
     render(): JSX.Element[] {
         const { childNodes, className } = this.props;
+        const { collapsed } = this.state;
 
         if (childNodes && childNodes.length) {            
             return childNodes.map(node => (
                 <ul key={node.name} className={`nav-item ${className}`}>
-                    <Link className="nav-link" to={`/${node.urlPath}`}>{node.name}</Link>
+                    <div className="row no-gutters pt-1 pb-1">
+                        
+                        <span className="col-1 text-center">
+                        {(node.children && node.children.length) 
+                            ? <FontAwesomeIcon icon={collapsed ? faPlus : faMinus} size="sm"/>
+                            : null}
+                        </span>
+                        <Link className="col-11 nav-link pt-0 pb-1" to={`/${node.urlPath}`}>{node.name}</Link>
+                    </div>
+                    
                     {(node.children && node.children.length)
-                        ? (
-                            <TreeNode childNodes={node.children} className="pl-3"/>
-                        )
+                        ? <TreeNode childNodes={node.children} className={`pl-4 ${collapsed && "tv-collapsed"}`}/>
                         : null}
                 </ul>
             ));
