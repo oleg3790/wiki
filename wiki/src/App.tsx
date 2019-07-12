46597dc294a6c10f5ec670d42fb400d9e6e068e0
Oleg Krysko <oleg.krysko@outlook.com>
2019-07-11 21:40:07 -0500

    Added tree view components

--- wiki/src/App.tsx
+++ wiki/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './styles/App.scss';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Layout from './Layout';
 import GitHubContentService from './commons/GitHubContentService';
-import { mapRoutes } from './commons/ContentTreeMapper';
 import { IContentTree } from './commons/ContentTree';
+import NodeDetails from './commons/NodeDetails';
 
 interface IAppState {
     contentTree: IContentTree;
@@ -24,13 +24,30 @@ export default class App extends React.Component<any, IAppState> {
         this.setState({ contentTree: tree });
     }
 
+    /**
+     * Recursively maps routes, using the content tree
+     * @param content 
+     * @param initialRoutes Any initial routes to be passed 
+     */
+    mapRoutes = (contentTree: IContentTree, initialRoutes: JSX.Element[] = []): JSX.Element[] => {
+        contentTree.children.forEach(node => {
+            if (node.downloadUrl && node.urlPath) {
+                initialRoutes.push(
+                    <Route key={node.urlPath} path={`/${node.urlPath}`} 
+                        render={(routeProps) => <NodeDetails {...routeProps} contentUrl={node.downloadUrl}/>}/>);
+            }        
+            return this.mapRoutes(node, initialRoutes);
+        });
+        return initialRoutes;
+    }
+
     render() {
         const { contentTree } = this.state;
         return (
             <HashRouter basename="/">
                 <Layout contentTree={contentTree}>
                     <div>
-                        {contentTree && mapRoutes(contentTree)}
+                        {contentTree && this.mapRoutes(contentTree)}
                     </div>
                 </Layout>
             </HashRouter>
--- wiki/src/commons/tree-view/TreeView.tsx
+++ wiki/src/commons/tree-view/TreeView.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import ContentTree from '../ContentTree';
+import TreeNode from './TreeNode';
 
 
 interface ITreeViewProps {
@@ -7,10 +8,19 @@ interface ITreeViewProps {
 }
 
 export default class TreeView extends React.Component<ITreeViewProps, {}> {
+    constructor(props: ITreeViewProps) {
+        super(props);
+    }
+
     render() {
+        const { contentTree } = this.props;
+
         return (
             <div>
-                
+                {(contentTree && contentTree.children && contentTree.children.length)
+                    ? <TreeNode childNodes={contentTree.children} className="pl-0"/>
+                    : null
+                }
             </div>
         );
     }
