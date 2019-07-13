3fabee9393df034dff73572763280dce86d85f74
Oleg Krysko <oleg.krysko@outlook.com>
2019-07-12 22:27:16 -0500

    Change tree node collapse icons

--- wiki/src/commons/tree-view/TreeNode.tsx
+++ wiki/src/commons/tree-view/TreeNode.tsx
@@ -4,7 +4,7 @@ import '../../styles/App.scss';
 import { Link } from 'react-router-dom';
 import ContentTree from '../ContentTree';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 interface ITreeNodeProps {
     node: ContentTree;
@@ -38,7 +38,7 @@ export default class TreeNode extends React.Component<ITreeNodeProps, ITreeNodeS
                     <div className="row no-gutters pt-1 pb-1">
                         <span className="col-1 text-center" onClick={this._handleCollapseToggle}>
                         {(node.children && node.children.length) 
-                            ? <FontAwesomeIcon icon={collapsed ? faPlus : faMinus} size="sm"/>
+                            ? <FontAwesomeIcon icon={collapsed ? faChevronRight : faChevronDown} size="sm"/>
                             : null}
                         </span>
                         <Link className="col-11 nav-link pt-0 pb-1" to={`/${node.urlPath}`}>{node.name}</Link>
