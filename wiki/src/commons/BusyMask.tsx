a8459351a1453cf6ca4d2d75397ee22bddae08d9
Oleg Krysko <oleg.krysko@outlook.com>
2019-07-15 22:55:40 -0500

    Add busy mask and loading element

---
+++ wiki/src/commons/BusyMask.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import '../styles/App.scss';
+import '../styles/Loading.scss';
+
+const BusyMask = (props: any) => {
+    return (
+        <div id="busy-mask">
+            <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+        </div>
+    );
+}
+
+export default BusyMask;
